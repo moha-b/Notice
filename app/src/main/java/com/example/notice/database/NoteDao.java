9f7c12cd28cd6bc869e3f2f927c45c2b72ab5f2c
hab <Mohabzax4@gmail.com>
2023-04-07 01:41:22 +0200

    initialize database

---
+++ app/src/main/java/com/example/notice/database/NoteDao.java
@@ -0,0 +1,30 @@
+package com.example.notice.database;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+import com.example.notice.entities.Note;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Note note);
+
+    @Update
+    void update(Note note);
+
+    @Delete
+    void delete(Note note);
+
+    @Query("SELECT * FROM notes")
+    List<Note> getAllNotes();
+
+    @Query("SELECT * FROM notes WHERE id = :noteId")
+    Note getNoteById(int noteId);
+}
