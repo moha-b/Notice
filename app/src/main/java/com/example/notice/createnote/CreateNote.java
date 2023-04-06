69c8ce65a129fc60147b2a61e823ee047e480439
hab <Mohabzax4@gmail.com>
2023-04-06 19:31:41 +0200

    Create Note Activity Layout

---
+++ app/src/main/java/com/example/notice/createnote/CreateNote.java
@@ -0,0 +1,14 @@
+package com.example.notice.createnote;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.notice.R;
+
+public class CreateNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+    }
+}
